3a3c64bdd56285bae18adbf7527dd08aee1aa50e
Anton Plyshevskiy <happysensey@gmail.com>
2016-11-04 19:27:18 +0300

    Created project and StepTable class with some fields

---
+++ Simplex/StepTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplex
+{
+    public enum FunctionType
+    {
+        Maximum, Minimum
+    }
+
+    public class StepTable
+    {
+        public int Index { get; set; }
+
+        public int UnknownQuantity { get; set; }
+        public int RestrictionQuantity { get; set; }
+
+        public double[] MainCoefficient { get; set; }
+        public double[] FreeMember { get; set; }
+        public double[] BasisIndex { get; set; }
+        public double[][] RestrictionCoefficient { get; set; }
+
+        public double Value { get; set; }
+
+        public double[] Delta { get; set; }
+        public double[] Theta { get; set; }
+    }
+}
