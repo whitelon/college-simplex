a6addafc5f93b60d9d445bf0b716efdd3e4118a9
Anton Plyshevskiy <happysensey@gmail.com>
2016-11-06 15:12:34 +0300

    Created FindValue() method and test

---
+++ Simplex.Test/StepTableTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Simplex.Test
+{
+    [TestClass]
+    public class StepTableTests
+    {
+        List<Simplex.StepTable> Tables = new List<Simplex.StepTable>
+            (
+            new[] 
+            {
+                new Simplex.StepTable
+                (
+                    mainCoefficient: new double[] { 2, 3, 0, 0 },
+                    basisIndex: new int[] { 2, 3 },
+                    freeMember: new double[] { 2, 4 },
+                    restrictionCoefficient: new double[][] 
+                    {
+                        new double[] { 1, 1, 1, 0 },
+                        new double[] { 1, -1, 0, 1 }
+                    }
+                ),
+                new Simplex.StepTable
+                (
+                    mainCoefficient: new double[] { 2, 3, 0, 0 },
+                    basisIndex: new int[] { 1, 3 },
+                    freeMember: new double[] { 2, 6 },
+                    restrictionCoefficient: new double[][]
+                    {
+                        new double[] { 1, 1, 1, 0 },
+                        new double[] { 2, 0, 1, 1 }
+                    }
+                )
+            }
+            );
+        [TestMethod]
+        public void TestingFindValueMethod()
+        {
+            Assert.AreEqual(Tables[0].Value, 0);
+            Assert.AreEqual(Tables[1].Value, 6);
+        }
+    }
+}
--- Simplex/StepTable.cs
+++ Simplex/StepTable.cs
@@ -15,17 +15,45 @@ namespace Simplex
     {
         public int Index { get; set; }
 
-        public int UnknownQuantity { get; set; }
-        public int RestrictionQuantity { get; set; }
+        public int UnknownQuantity {
+            get
+            {
+                return MainCoefficient.Length;
+            }
+        }
+        public int RestrictionQuantity {
+            get
+            {
+                return BasisIndex.Length;
+            }
+        }
 
         public double[] MainCoefficient { get; set; }
         public double[] FreeMember { get; set; }
-        public double[] BasisIndex { get; set; }
+        public int[] BasisIndex { get; set; }
         public double[][] RestrictionCoefficient { get; set; }
 
-        public double Value { get; set; }
+        public double Value { get; private set; }
 
         public double[] Delta { get; set; }
         public double[] Theta { get; set; }
+
+        public StepTable(double[] mainCoefficient, int[] basisIndex, double[] freeMember, double[][] restrictionCoefficient)
+        {
+            MainCoefficient = mainCoefficient;
+            BasisIndex = basisIndex;
+            FreeMember = freeMember;
+            RestrictionCoefficient = restrictionCoefficient;
+            Value = FindValue();
+        }
+
+        private double FindValue()
+        {
+            return FreeMember
+                .Select((e, i) =>  e * MainCoefficient[BasisIndex[i]])
+                .Aggregate((f, s) => f + s);
+        }
+
+        
     }
 }
