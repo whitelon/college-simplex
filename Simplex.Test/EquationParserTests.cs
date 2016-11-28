ad292d8826225319d029f9130b771fcbec7fcaca
Anton Plyshevskiy <happysensey@gmail.com>
2016-11-29 01:02:28 +0300

    Added EquationParser and tests

---
+++ Simplex.Test/EquationParserTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simplex;
+using System.Linq;
+namespace Simplex.Test
+{
+    [TestClass]
+    public class EquationParserTests
+    {
+        [TestMethod]
+        public void MainFuncTest()
+        {
+            Assert.IsTrue(EquatonParser.ParseMainFunction("-20x1 - 10x2 + 123x3").SequenceEqual(new double[] { -20, -10, +123 }));
+
+
+            var tuple = EquatonParser.ParseRestriction("-20x1 - 10x2 + 12,3x3 = 100");
+            Assert.AreEqual(100, tuple.Item2);
+            Assert.IsTrue(tuple.Item1.SequenceEqual(new double[] { -20, -10, +12.3 }));
+        }
+    }
+}
---
+++ Simplex/EquatonParser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Simplex
+{
+    public static class EquatonParser
+    {
+        static readonly Regex _mainFunctionRegex = new Regex(@"(([+\-]?((?:\d*,)?\d+))[xX](\d+))+");
+        static readonly Regex _restrictionRegex = new Regex(@"((([+\-]?((?:\d*,)?\d+))[xX](\d+))+)=((?:\d*,)?\d+)");
+        static readonly Regex _memberRegex = new Regex(@"(([+\-]?((?:\d*,)?\d+))[xX](\d+))");
+
+        
+        public static double[] ParseMainFunction(string func)
+        {
+            func = func.Replace(" ", "");
+            if (!_mainFunctionRegex.IsMatch(func)) throw new Exception("Wrong main func format");
+            var members = _memberRegex.Matches(func);
+            var coeffs = new Dictionary<int, double>();
+            foreach (Match member in members)
+            {
+                coeffs.Add(int.Parse(member.Groups[4].Value), double.Parse(member.Groups[2].Value));
+            }
+            var MainCoefficient = new double[coeffs.Keys.Max()];
+            foreach (var coeff in coeffs) MainCoefficient[coeff.Key - 1] = coeff.Value;
+            return MainCoefficient;
+        }
+        public static Tuple<double[], double> ParseRestriction(string restr)
+        {
+            restr = restr.Replace(" ", "");
+            if (!_restrictionRegex.IsMatch(restr)) throw new Exception("Wrong restriction format");
+
+            var freeMember = double.Parse(_restrictionRegex.Match(restr).Groups[6].Value);
+            var coeffs = ParseMainFunction(_restrictionRegex.Match(restr).Groups[1].Value);
+
+            return new Tuple<double[], double>(coeffs, freeMember);
+        }
+
+
+    }
+}
